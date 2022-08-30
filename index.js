81d32f18eee4d68c3f6545db56025365863d7847
AlexGallardo <alex.ibaceta@gmail.com>
2022-08-30 00:40:46 -0400

    primer servidor con express, definicion de rutas con con app.get res.send, res.json

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+const express = require('express');
 console.log('my App');
+
+const app = express();
+const port=3000;
+
+app.get('/',(req, res)=>{
+    res.send('Hola mi Server en express');
+})
+app.get('/nueva_ruta',(req, res)=>{
+    res.send('Hola soy una nueva ruta');
+})
+
+app.get('/products',(req, res)=>{
+    res.json({
+      name:"Product 1",
+      price:1000
+    })
+})
+
+app.listen(port,()=>{
+    console.log('Mi puerto:', port);
+})
